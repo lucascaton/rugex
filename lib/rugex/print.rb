bf29db40020b18c599f3deaeaf64ee2e03a5ccef
Lucas Caton <lucascaton@gmail.com>
2011-02-06 16:48:35 -0200

    Improve text output colors

--- lib/rugex/print.rb
+++ lib/rugex/print.rb
@@ -5,7 +5,7 @@ module Rugex
     def print_colored_text
       colors = {
         :white => "\033[0;37m",
-        :red   => "\033[0;31m"
+        :red   => "\033[0;31m\033[4m"
       }
       @text.gsub(@regex,"#{colors[:red]}\\&#{colors[:white]}")
     end
