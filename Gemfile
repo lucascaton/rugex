0df00782d7e0000bc5f5de8ea50ba637bea85610
Lucas Caton <lucascaton@gmail.com>
2011-02-06 16:04:44 -0200

    Remove thor (gem dependency)

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'http://rubygems.org'
 
-gem 'thor', '>= 0.14.6'
-
 group :development do
   gem 'rspec', '~> 2.3.0'
   gem 'bundler', '~> 1.0.9'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'rugex'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
 end
